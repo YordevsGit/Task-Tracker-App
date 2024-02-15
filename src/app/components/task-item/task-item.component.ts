53a38ea784eb9c60e6dd10c096b3a484b97ccd91
Roy Bright Asiku <roybrightasiku@gmail.com>
2024-02-15 13:15:06 +0000

    First commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  @Input() task:any;
+}
